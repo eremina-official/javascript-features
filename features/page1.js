7708cf99b8abbf6f4b101010cba0ab8a73642661
Marina Eremina <eremina.jobmail@gmail.com>
2019-05-11 18:24:47 +0200

    add variable scoping exercise

--- features/page1.js
+++ features/page1.js
@@ -22,7 +22,26 @@ const testFunction2 = function() {
 testFunction2();
 
 
-console.log('--------------');
+console.log('------- variable scoping -------');
 
-let datenow = new Date();
-console.log(datenow);
+/* 
+  let and const are block-scoped variables, that they are not hoisted like var.
+  When a variable is declared with let, it can be reassigned.
+  When a variable is declared with const, it can not be reassigned.
+  When an object is declared with const, it can not be reassigned, but the properties 
+  and methods of the object can be changed.
+*/
+
+const buttonsI = document.querySelectorAll('div.i button');
+var i;
+for (i = 0; i < 3; i++) {
+  buttonsI[i].addEventListener('click', () => { console.log('i', i) });
+}
+
+const buttonsJ = document.querySelectorAll('div.j button');
+for (let j = 0; j < 3; j++) {
+  buttonsJ[j].addEventListener('click', () => { console.log('j', j) });
+}
+
+console.log('------------');
+
