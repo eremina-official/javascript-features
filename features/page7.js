1aa9835b08c59247713cd7c87a0a05bb6521c7da
Marina Eremina <eremina.jobmail@gmail.com>
2019-09-30 22:33:55 +0200

    add reduce exercise

--- features/page7.js
+++ features/page7.js
@@ -10,3 +10,16 @@ console.log('addConcat', addConcat);
 const concatAdd = pipe(concat('c'), add)('a');
 console.log('concatAdd', concatAdd);
 
+console.log('----- reduce -----');
+
+const emptyArray = [[1], [], []];
+
+const reducedArray = emptyArray.reduce((acc, currentValue, currentValueIndex) => {
+  if (currentValue) {
+    acc[currentValueIndex] = [1];
+  }
+  return acc;
+}, emptyArray);
+
+console.log('emptyArray', emptyArray);
+console.log('reducedArary', reducedArray);
