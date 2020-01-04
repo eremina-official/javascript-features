a7f8f82d2ccd1dc5397e4d614479bb0ba10fd653
Marina Eremina <eremina.jobmail@gmail.com>
2020-01-04 22:53:39 +0100

    update examples

--- features/page27.js
+++ features/page27.js
@@ -1,6 +1,13 @@
 console.log('----- async await -----');
 //example borrowed from mdn https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
 
+//async function returns a Promise, therefore it is possible to chain .then method to it
+async function showNumber() {
+  return 'number';
+}
+showNumber().then(data => { console.log(data) });
+
+
 function resolveAfter2Seconds() {
   return new Promise(resolve => {
     console.log('before setTimeout');
@@ -14,9 +21,9 @@ function resolveAfter2Seconds() {
 async function asyncCall() {
   console.log('calling');
   const result = await resolveAfter2Seconds();
-  console.log(result);
-  // expected output: 'resolved'
+  console.log(result); //the value assigned to result will be 'resolved'
   console.log('after result');
 }
 
 asyncCall();
+console.log('after asyncCall')
